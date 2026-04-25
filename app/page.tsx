e55e60d375c1df51038f9e5cfe23d9590fa32121
Victor Borges LinuxDell <tedborges.atendimento@gmail.com>
2026-04-25 19:45:10 -0300

    feat: add Header components and banner and images

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,32 @@
 "use client"
 
+import { SearchIcon } from "lucide-react"
+import Header from "./_components/ui/header"
+import { Input } from "./_components/ui/input"
 import { Button } from "./_components/ui/button"
+import Image from "next/image"
 
 const Home = () => {
-  return <Button>Home</Button>
+  return (
+    <div>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Victor</h2>
+        <p>Sexta-feira, 28 de março de 2026</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..."/>
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative w-full h-37.5 mt-6">
+          <Image src="/banner-01.png" alt="Agende nos melhores com FSW" className="object-cover rounded-xl" fill/>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Home
